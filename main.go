f3d1786737d7f263ee09203e202b003bbbeaa4ed
Rafael Peroco <rafaelperoco@gmail.com>
2022-11-08 15:35:45 -0300

    fix generation

--- main.go
+++ main.go
@@ -1,3 +1,8 @@
+// Language: go
+// A CLI tool to generate passwords with entropy and complexity
+// generate passwords just with letters and numbers or with special characters (Default)
+// use cobra for CLI
+
 package main
 
 import (
@@ -10,48 +15,87 @@ import (
 )
 
 var (
-	size     int
-	specials bool
+	// letters and numbers
+	letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+	// letters, numbers and special characters
+	lettersAndSpecial = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+{}|:<>?`~-=[]\\;',./")
+
+	// flags
+	lettersFlag bool
+	specialFlag bool
+	lengthFlag  int
 )
 
-func init() {
-	size = 16
-	specials = false
-}
-
 func main() {
-	rootCmd := &cobra.Command{
-		Use:   "password",
-		Short: "Password generator",
-		Long:  "A password generator cli with high entropy",
+	// root command
+	var rootCmd = &cobra.Command{
+		Use:   "passgen",
+		Short: "A CLI tool to generate passwords with entropy and complexity",
+		Long:  `A CLI tool to generate passwords with entropy and complexity`,
 		Run: func(cmd *cobra.Command, args []string) {
-			password := generatePassword(size, specials)
-
+			// generate password
+			password := generatePassword()
+			// print password
 			fmt.Println(password)
 		},
 	}
 
-	rootCmd.Flags().IntVarP(&size, "size", "s", 16, "size of password")
-	rootCmd.Flags().BoolVarP(&specials, "specials", "S", false, "add special characters")
+	// flags
+	rootCmd.Flags().BoolVarP(&lettersFlag, "letters", "l", false, "use letters and numbers")
+	rootCmd.Flags().BoolVarP(&specialFlag, "special", "s", false, "use letters, numbers and special characters")
+	rootCmd.Flags().IntVarP(&lengthFlag, "length", "n", 20, "length of the password")
 
+	// execute command
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 }
 
-func generatePassword(size int, specials bool) string {
+// generatePassword generates a password with entropy and complexity
+func generatePassword() string {
+	// seed random
 	rand.Seed(time.Now().UnixNano())
 
-	var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	if specials {
-		chars += "!@#$%^&*()_+-=[]{}|;:,.<>?"
+	// check flags
+	if lettersFlag {
+		// generate password with letters and numbers
+		return generatePasswordWithLetters()
+	} else if specialFlag {
+		// generate password with letters, numbers and special characters
+		return generatePasswordWithLettersAndSpecial()
+	} else {
+		// generate password with letters, numbers and special characters
+		return generatePasswordWithLettersAndSpecial()
 	}
-
-	var password string
-	for i := 0; i < size; i++ {
-		password += string(chars[rand.Intn(len(chars))])
-	}
-
-	return password
 }
+
+// generatePasswordWithLetters generates a password with letters and numbers
+func generatePasswordWithLetters() string {
+	// password
+	password := make([]rune, lengthFlag)
+
+	// generate password
+	for i := range password {
+		password[i] = letters[rand.Intn(len(letters))]
+	}
+
+	// return password
+	return string(password)
+}
+
+// generatePasswordWithLettersAndSpecial generates a password with letters, numbers and special characters
+func generatePasswordWithLettersAndSpecial() string {
+	// password
+	password := make([]rune, lengthFlag)
+
+	// generate password
+	for i := range password {
+		password[i] = lettersAndSpecial[rand.Intn(len(lettersAndSpecial))]
+	}
+
+	// return password
+	return string(password)
+}
+
+// use: go run main.go -h
