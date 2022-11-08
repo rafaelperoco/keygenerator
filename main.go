7cf90b577a576fdde83314f0cd9ca67e430fdd4b
Rafael Peroco <rafaelperoco@gmail.com>
2022-11-08 15:56:57 -0300

    fix generation

--- main.go
+++ main.go
@@ -1,8 +1,3 @@
-// Language: go
-// A CLI tool to generate passwords with entropy and complexity
-// generate passwords just with letters and numbers or with special characters (Default)
-// use cobra for CLI
-
 package main
 
 import (
@@ -15,87 +10,77 @@ import (
 )
 
 var (
-	// letters and numbers
 	letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
-	// letters, numbers and special characters
 	lettersAndSpecial = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+{}|:<>?`~-=[]\\;',./")
 
-	// flags
 	lettersFlag bool
 	specialFlag bool
 	lengthFlag  int
+	excludeFlag string
 )
 
 func main() {
-	// root command
 	var rootCmd = &cobra.Command{
-		Use:   "passgen",
+		Use:   "pwdgen",
 		Short: "A CLI tool to generate passwords with entropy and complexity",
 		Long:  `A CLI tool to generate passwords with entropy and complexity`,
 		Run: func(cmd *cobra.Command, args []string) {
-			// generate password
-			password := generatePassword()
-			// print password
-			fmt.Println(password)
+			if excludeFlag != "" {
+				fmt.Println(excludeCharacters(generatePassword()))
+			} else {
+				fmt.Println(generatePassword())
+			}
 		},
 	}
-
-	// flags
 	rootCmd.Flags().BoolVarP(&lettersFlag, "letters", "l", false, "use letters and numbers")
 	rootCmd.Flags().BoolVarP(&specialFlag, "special", "s", false, "use letters, numbers and special characters")
 	rootCmd.Flags().IntVarP(&lengthFlag, "length", "n", 20, "length of the password")
+	rootCmd.Flags().StringVarP(&excludeFlag, "exclude", "e", "", "exclude characters from the password")
 
-	// execute command
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 }
 
-// generatePassword generates a password with entropy and complexity
 func generatePassword() string {
-	// seed random
 	rand.Seed(time.Now().UnixNano())
 
-	// check flags
+	var password []rune
 	if lettersFlag {
-		// generate password with letters and numbers
-		return generatePasswordWithLetters()
+		password = make([]rune, lengthFlag)
+		for i := range password {
+			password[i] = letters[rand.Intn(len(letters))]
+		}
 	} else if specialFlag {
-		// generate password with letters, numbers and special characters
-		return generatePasswordWithLettersAndSpecial()
+		password = make([]rune, lengthFlag)
+		for i := range password {
+			password[i] = lettersAndSpecial[rand.Intn(len(lettersAndSpecial))]
+		}
 	} else {
-		// generate password with letters, numbers and special characters
-		return generatePasswordWithLettersAndSpecial()
+		password = make([]rune, lengthFlag)
+		for i := range password {
+			password[i] = lettersAndSpecial[rand.Intn(len(lettersAndSpecial))]
+		}
 	}
-}
-
-// generatePasswordWithLetters generates a password with letters and numbers
-func generatePasswordWithLetters() string {
-	// password
-	password := make([]rune, lengthFlag)
-
-	// generate password
-	for i := range password {
-		password[i] = letters[rand.Intn(len(letters))]
-	}
-
-	// return password
 	return string(password)
 }
 
-// generatePasswordWithLettersAndSpecial generates a password with letters, numbers and special characters
-func generatePasswordWithLettersAndSpecial() string {
-	// password
-	password := make([]rune, lengthFlag)
-
-	// generate password
-	for i := range password {
-		password[i] = lettersAndSpecial[rand.Intn(len(lettersAndSpecial))]
+func excludeCharacters(password string) string {
+	var result string
+	for _, c := range password {
+		if !contains(excludeFlag, c) {
+			result += string(c)
+		}
 	}
-
-	// return password
-	return string(password)
+	return result
 }
 
-// use: go run main.go -h
+func contains(s string, c rune) bool {
+	for _, r := range s {
+		if r == c {
+			return true
+		}
+	}
+	return false
+}
